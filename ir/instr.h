26a1840218af4e8cae7f82589f96dd00ea565a01
Ramkumar Ramachandra <ramkumar.ramachandra@codasip.com>
2024-12-11 11:37:30 +0000

    FakeShuffle: add FakeShuffle, required by Minotaur

--- ir/instr.h
+++ ir/instr.h
@@ -1307,4 +1307,21 @@ public:
 
 const ConversionOp *isCast(ConversionOp::Op op, const Value &v);
 Value *isNoOp(const Value &v);
+
+class FakeShuffle final : public Instr {
+  Value *v1, *v2, *mask;
+
+public:
+  FakeShuffle(Type &type, std::string &&name, Value &v1, Value &v2, Value &mask)
+      : Instr(type, std::move(name)), v1(&v1), v2(&v2), mask(&mask) {}
+  std::vector<Value *> operands() const override;
+  bool propagatesPoison() const override;
+  bool hasSideEffects() const override;
+  void rauw(const Value &what, Value &with) override;
+  void print(std::ostream &os) const override;
+  StateValue toSMT(State &s) const override;
+  smt::expr getTypeConstraints(const Function &f) const override;
+  std::unique_ptr<Instr> dup(Function &f,
+                             const std::string &suffix) const override;
+};
 }
